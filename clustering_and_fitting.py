1accd84bafd87955a31119a1e4f14b1c651e4241
Interloper03 <149250321+Interloper03@users.noreply.github.com>
2025-11-23 18:34:38 +0000

    Update clustering_and_fitting.py

--- clustering_and_fitting.py
+++ clustering_and_fitting.py
@@ -1,14 +1,3 @@
-"""
-This is the template file for the clustering and fitting assignment.
-You will be expected to complete all the sections and
-make this a fully working, documented file.
-You should NOT change any function, file or variable names,
- if they are given to you here.
-Make use of the functions presented in the lectures
-and ensure your code is PEP-8 compliant, including docstrings.
-Fitting should be done with only 1 target variable and 1 feature variable,
-likewise, clustering should be done with only 2 variables.
-"""
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
